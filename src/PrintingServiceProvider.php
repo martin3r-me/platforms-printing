d8c1b6c36d4d228701c1acf56b98eaf449fd3ae2
Martin Erren <martin3r@Mac.fritz.box>
2025-09-12 13:50:49 +0200

    ui

--- src/PrintingServiceProvider.php
+++ src/PrintingServiceProvider.php
@@ -54,15 +54,15 @@ class PrintingServiceProvider extends ServiceProvider
             ]);
         }
 
-        // API Routes für CloudPRNT - außerhalb des Modul-Systems registrieren
-        if (config('printing.api.cloudprnt.enabled')) {
-            $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
-        }
-
-        // Web-Routen nur laden, wenn das Modul registriert wurde
+        // Web-Routen und API-Routen nur laden, wenn das Modul registriert wurde
         if (PlatformCore::getModule('printing')) {
             ModuleRouter::group('printing', function () {
                 $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+                
+                // API Routes für CloudPRNT
+                if (config('printing.api.cloudprnt.enabled')) {
+                    $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+                }
             });
         }
 
